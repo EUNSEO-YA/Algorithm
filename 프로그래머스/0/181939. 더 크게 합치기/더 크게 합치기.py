014c5cf1e5b969b0393dd6bbea9c98c93005f670
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2024-07-10 15:51:54 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,18 +1,6 @@
 def solution(a, b):
-    answer = 0
-    
-    a = str(a)
-    b = str(b)
-    
-    str1 = a + b
-    str2 = b + a
-    
-    str1 = int(str1)
-    str2 = int(str2)
-    
-    if str1 >= str2:
-        answer = str1
-    else:
-        answer = str2
-    
-    return answer
+	
+	str1 = int(str(a) + str(b))
+	str2 = int(str(b) + str(a))
+	
+	return max(str1, str2)
