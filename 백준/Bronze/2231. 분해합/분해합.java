46116d2bf5b97d1602f7f86b9f0228baf75dfc83
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2025-07-07 23:37:59 +0900

    [Bronze II] Title: 분해합, Time: 176 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result = 0;
+
+        int start = Math.max(1, N - 9 * String.valueOf(N).length());
+
+        for (int i = start; i < N; i++) {
+            int sum = i;
+            int temp = i;
+
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            if (sum == N) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
