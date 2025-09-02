711e466324d02f0ac6c03ff1318a991323a243cb
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2025-09-02 11:31:37 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 1.13 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,25 +1,32 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int[] cards) {        
-        boolean[] visited = new boolean[cards.length];
-        List<Integer> sizes = new ArrayList<>();
+    public int solution(int[] cards) {
+        int num1 = 0;
+        int num2 = 0;
         
-        for(int i = 0; i < cards.length; i++) {
-            if(!visited[i]) {
-                int count = 0;
-                int cur = i;
-                
-                while(!visited[cur]) {
-                    visited[cur] = true;
-                    count++;
-                    cur = cards[cur] - 1;
-                }
-                if(count > 0) sizes.add(count);
+        boolean[] visited = new boolean[cards.length];
+        
+        int idx = 0;
+        int re = 0;
+        
+        while(visited[idx] == true) {
+            num1++;
+            visited[idx] = true;
+            idx = cards[idx];
+        }
+        
+        for(int i = 0; i < visited.length; i++) {
+            if(visited[i] == false) {
+                re = i;
+                break;
             }
         }
-        if(sizes.size() < 2) return 0;
-        sizes.sort(Collections.reverseOrder());
-        return sizes.get(0) * sizes.get(1);
+        
+        while(visited[re] == true) {
+            num2++;
+            visited[re] = true;
+            re = cards[re];
+        }
+        
+        return num1 * num2;
     }
 }
