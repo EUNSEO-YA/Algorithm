bf29f213ce0a187795c26e01e52df7414309d028
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2025-09-17 11:24:03 +0900

    [Bronze I] Title: 평균, Time: 200 ms, Memory: 18856 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,26 +3,25 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] score = new int[n];
+        int N = sc.nextInt();
+        int[] score = new int[N];
 
-        double sum = 0;
-        double avg = 0;
-
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < N; i++) {
             score[i] = sc.nextInt();
         }
 
         Arrays.sort(score);
-        int max = score[n - 1];
+        int M = score[N - 1];
+        double sum = 0;
+        double answer = 0;
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < N; i++) {
             sum += score[i];
         }
 
-        sum = sum / max * 100;
-        avg = sum / n;
+        sum = sum / M * 100;
+        answer = sum / N;
 
-        System.out.println(avg);
+        System.out.println(answer);
     }
 }
