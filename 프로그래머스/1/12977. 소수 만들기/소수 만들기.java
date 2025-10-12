b0669a48243bc8e9b478e1b550bc6eea24a7fac4
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2025-10-12 19:19:38 +0900

    [level 1] Title: 소수 만들기, Time: 13.12 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                for(int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    boolean isPrime = true;
+
+                    for(int s = 2; s <= Math.sqrt(sum); s++) {
+                        if(sum % s == 0) isPrime = false;
+                    }
+                    
+                    if(isPrime == true) answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
