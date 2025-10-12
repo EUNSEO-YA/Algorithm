4e01785507380ebf7c03297c5651fab8ffe768f1
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2025-10-12 18:46:22 +0900

    [level 1] Title: 덧칠하기, Time: 1.46 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,23 +1,12 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int[] area = new int[n + 1];
-        int paint = 1;
+        int lastpainted = 0;
         
         for(int i = 0; i < section.length; i++) {
-            area[section[i]] = 1;
-        }
-        
-        while(paint <= n) {
-            if(area[paint] == 0) paint++;
-            else {
+            if(section[i] > lastpainted) {
                 answer++;
-                if(paint + m > n) {
-                    break;
-                }
-                else {
-                    paint += m;
-                }
+                lastpainted = section[i] + m - 1;
             }
         }
         
