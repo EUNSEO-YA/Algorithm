671cf950d9b92fdae6b14489fff24eec6d980fcd
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2025-10-29 20:20:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,21 +2,20 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         StringBuilder sb = new StringBuilder();
+        int num = 1;
         
         while(n > 0) {
-            sb.append(n % 3);
+            int plus = n % 3;
+            sb.append(plus);
             n /= 3;
         }
         
         sb.reverse();
         
-        int mul = 1;
-        
         for(int i = 0; i < sb.length(); i++) {
-            long num = sb.charAt(i) - '0';
-            
-            answer += (mul * num);
-            mul *= 3;
+            int now = sb.charAt(i) - '0';
+            answer += (now * num);
+            num *= 3;
         }
         
         return answer;
