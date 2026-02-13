dec13195c17ca1947423e06c0b4b34fe2f2547fb
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2026-02-13 09:48:21 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22172 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,37 +1,52 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-    static int n, m;
-    static boolean[] visited;
-    static int[] result;
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+public class Main {
+	
+	static int N, M;
+	static int[] result;
+	static boolean[] selected;
+	static StringBuilder sb = new StringBuilder();
 
-        visited = new boolean[n + 1];
-        result = new int[m];
-
-        dfs(0);
-    }
-
-    static void dfs(int depth) {
-        if(depth == m) {
-            for(int num : result) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for(int i = 1; i <= n; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                result[depth] = i;
-                dfs(depth + 1);
-                visited[i] = false;
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		result = new int[M];
+		selected = new boolean[N + 1];
+		
+		dfs(0);
+		
+		System.out.println(sb);
+	}
+	
+	static void dfs(int cnt) {
+		
+		// 1. 기저 조건
+		if(cnt == M) {
+			for(int i = 0; i < result.length; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		// 2. 재귀 유도
+		for(int i = 1; i <= N; i++) {
+			if(!selected[i]) {
+				selected[i] = true;
+				result[cnt] = i;
+				
+				dfs(cnt + 1);
+				
+				selected[i] = false;
+			}
+		}
+		
+	}
+	
 }
