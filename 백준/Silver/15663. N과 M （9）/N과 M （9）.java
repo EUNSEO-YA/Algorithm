9bdeacb9fde1db5dd37ef8f87f46492c27308a53
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2026-02-18 17:06:19 +0900

    [Silver II] Title: N과 M (9), Time: 184 ms, Memory: 21236 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] result;
+	static int[] input;
+	
+	static boolean[] selected;
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		result = new int[M];
+		input = new int[N];
+		selected = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input);
+		
+		dfs(0);
+		System.out.println(sb);
+	}
+	
+	static void dfs(int idx) {
+		if(idx == M) {
+			for(int i = 0; i < result.length; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int before = -1;
+		
+		for(int i = 0; i < N; i++) {
+			if(!selected[i] && input[i] != before) {
+				selected[i] = true;
+				result[idx] = input[i];
+				before = input[i];
+				
+				dfs(idx + 1);
+				
+				selected[i] = false;
+			}
+		}
+	}
+}
