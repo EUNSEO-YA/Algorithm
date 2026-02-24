bb0349c2d8e9ecd82eba01670a30bde47ba56fdb
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2026-02-24 16:48:44 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,40 +1,27 @@
-import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        boolean[] visited = new boolean[N + 1];
-        Queue<int[]> queue = new LinkedList<>();
-
-        visited[N] = true;
-        queue.offer(new int[]{N, 0});
-
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int now = cur[0];
-            int count = cur[1];
-
-            if(now == 1) {
-                System.out.println(count);
-                return;
-            }
-
-            if(now % 3 == 0 && !visited[now / 3]) {
-                visited[now / 3] = true;
-                queue.offer(new int[]{now / 3, count + 1});
-            }
-            if(now % 2 == 0 && !visited[now / 2]) {
-                visited[now / 2] = true;
-                queue.offer(new int[]{now / 2, count + 1});
-            }
-            if(now - 1 >= 1 && !visited[now - 1]) {
-                visited[now - 1] = true;
-                queue.offer(new int[]{now - 1, count + 1});
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+		dp[1] = 0;
+		
+		for(int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + 1;
+			
+			if(i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+			
+			if(i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+		}
+		
+		System.out.println(dp[N]);
+	}
 }
