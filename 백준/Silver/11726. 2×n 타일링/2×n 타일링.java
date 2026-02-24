0909804c4dcb1bf8454d6746f1eac815fd96460c
Eunseo <103026637+EUNSEO-YA@users.noreply.github.com>
2026-02-24 17:17:44 +0900

    [Silver III] Title: 2×n 타일링, Time: 96 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        int[] dp = new int[1001];
 
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
-        }
-        
-        System.out.println(dp[n]);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[1001];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+		}
+		
+		System.out.println(dp[n]);
+	}
 }
